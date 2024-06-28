6dbdd03dfe8318f9e7b7570188f82e88263eda3d
VARSHITHA.N <116536589+vn2706@users.noreply.github.com>
2024-06-29 00:21:18 +0530

    Update Zomato_bangalore_model.py

--- Zomato_bangalore_model.py
+++ Zomato_bangalore_model.py
@@ -48,6 +48,22 @@ page_bg_img = '''
     ''' 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
+def predict_function(cuisine, location):
+    filtered_data = zomatoData[(zomatoData['cuisine'] == cuisine) & (zomatoData['location'] == location)]
+    if filtered_data.empty:
+        return "No restaurants found for the selected cuisine and location."
+
+    avg_price = round(filtered_data['price_for_one'].mean())
+    best_restaurant = filtered_data.loc[filtered_data['price_for_one'].idxmin()]
+    
+    result = {
+        "Standard Rate": avg_price,
+        "Restaurant Name": best_restaurant['restaurant_name'],
+        "Timings": best_restaurant['timings'],
+        "Surf To": best_restaurant['links']
+    }
+    return result
+
 def main():
     # Add heading and tagline
     st.markdown("<h1 style='text-align: center; color: gold;'>Zomato Recommendation System</h1>", unsafe_allow_html=True)
@@ -68,7 +84,15 @@ def main():
         Location = st.selectbox("", filterLocation)
 
         if st.button("Predict"):
-            st.write("Button clicked")
+            prediction_result = predict_function(Cuisine, Location)
+            
+            if isinstance(prediction_result, dict):
+                st.markdown("<span style='color: Olivedrab; font-family: \"Times New Roman\", Times, serif; font-weight: bold; font-size: 32px; margin-right: 10px;'>Standard Rate:</span><span style='color: purple; font-family: \"Georgia\", serif; font-weight: bold; font-size: 30px;'>{}</span>".format(prediction_result["Standard Rate"]), unsafe_allow_html=True)
+                st.markdown("<span style='color: Olivedrab; font-family: \"Times New Roman\", Times, serif; font-weight: bold; font-size: 32px; margin-right: 10px;'>Restaurant Name:</span><span style='color: purple; font-family: \"Georgia\", serif; font-weight: bold; font-size: 27px;'>{}</span>".format(prediction_result["Restaurant Name"]), unsafe_allow_html=True)
+                st.markdown("<span style='color: Olivedrab; font-family: \"Times New Roman\", Times, serif; font-weight: bold; font-size: 32px; margin-right: 10px;'>Timings:</span><span style='color: purple; font-family: \"Georgia\", serif; font-weight: bold; font-size: 27px;'>{}</span>".format(prediction_result["Timings"]), unsafe_allow_html=True)
+                st.markdown("<span style='color: Olivedrab; font-family: \"Times New Roman\", Times, serif; font-weight: bold; font-size: 32px; margin-right: 10px;'>Surf To:</span><span style='color: purple; font-family: \"Georgia\", serif; font-weight: bold; font-size: 27px;'><a href='{0}' style='color: purple;'>{0}</a></span>".format(prediction_result["Surf To"]), unsafe_allow_html=True)
+            else:
+                st.markdown("<span style='color: red;'>{}</span>".format(prediction_result), unsafe_allow_html=True)
 
     with tab2:
         st.markdown("<h2 style='text-align: center; color: teal;'>Newly Open Restaurants</h2>", unsafe_allow_html=True)
@@ -82,17 +106,27 @@ def main():
         st.markdown("<h2 style='font-size: 24px;'><span style='color: red;'><b>Restaurant Name:</b></span></h2>", unsafe_allow_html=True)
         selectedRestaurant = st.selectbox("", showRestaurantName)
 
+        if st.button("Confirm"):
+            restaurantInfo = showRestaurant[showRestaurant['restaurant_name'] == selectedRestaurant]
+            Estimate = restaurantInfo['price_for_one'].values[0]
+            dishes = restaurantInfo['cuisins'].values[0]
+            navigateTo = restaurantInfo['links'].values[0]
+
+            st.markdown("<span style='color: Olivedrab; font-family: \"Times New Roman\", Times, serif; font-weight: bold; font-size: 32px; margin-right: 10px;'>Estimate Price:</span><span style='color: purple; font-family: \"Georgia\", serif; font-weight: bold; font-size: 30px;'>{}</span>".format(Estimate), unsafe_allow_html=True)
+            st.markdown("<span style='color: Olivedrab; font-family: \"Times New Roman\", Times, serif; font-weight: bold; font-size: 32px; margin-right: 10px;'>Cuisine:</span><span style='color: purple; font-family: \"Georgia\", serif; font-weight: bold; font-size: 27px;'>{}</span>".format(dishes), unsafe_allow_html=True)
+            st.markdown("<span style='color: Olivedrab; font-family: \"Times New Roman\", Times, serif; font-weight: bold; font-size: 32px; margin-right: 10px;'>Navigate To:</span><span style='color: purple; font-family: \"Georgia\", serif; font-weight: bold; font-size: 27px;'><a href='{0}' style='color: purple;'>{0}</a></span>".format(navigateTo), unsafe_allow_html=True)
+
     with tab3:
-        st.markdown("<h2 style='text-align: center; color: blue;'>Feedback</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; color: red;'>Feedback</h2>", unsafe_allow_html=True)
+        name = st.text_input("Name")
+        feedback = st.text_area("Feedback")
         
-        name = st.text_input("Enter your name:")
-        feedback = st.text_area("Enter your feedback:")
-        
-        if st.button("Submit Feedback"):
+        if st.button("Submit"):
             save_feedback(name, feedback)
-            st.write("Feedback submitted successfully!")
+            st.success("Thank you for your feedback!")
 
 if __name__ == "__main__":
     main()
+
 
        
